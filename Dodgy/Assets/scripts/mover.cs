0c3d78bfa4a0062180e58ba8de2b6d0390d111c1
Kadir Cakir <ka.c0825@gmail.com>
2026-03-17 10:35:47 +0100

    push

--- Dodgy/Assets/scripts/mover.cs
+++ Dodgy/Assets/scripts/mover.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+   [SerializeField] float moveSpeed = 10f; 
+   
+   
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,10 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(1, 0, 0);
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float yValue = 0f;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        transform.Translate(xValue, yValue, zValue);
     }
 }
